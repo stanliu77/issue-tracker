e1edd70f0c165a298c3321619b6417605d517cfe
stanliu77 <stanliu778@gmail.com>
2024-08-15 13:41:54 +0800

    Start the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,26 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
+import classNames from 'classnames';
 
 const Navbar = () => {
   const navlist = [
     {name:"Dashboard",href:"/"},
     {name:"Issues",href:"/issues"}
   ]
+  const currentPath = usePathname()
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href="/"><AiFillBug /></Link>
       <ul className='flex space-x-6'>
         {navlist.map(nav=><Link id={nav.href} href={nav.href} 
-        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+        className= {classNames({
+          "text-zinc-500": currentPath !== nav.href,
+          "text-zinc-900": currentPath === nav.href,
+          "hover:text-zinc-800 transition-colors":true
+        })}
         >{nav.name}</Link>
         )}
       </ul>
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const issuesPage = () => {
+  return (
+    <div>
+      issuesPage
+    </div>
+  )
+}
+
+export default issuesPage
