6063c3622ae7a79e25c4ad124281036c8a8e7fe8
stanliu77 <stanliu778@gmail.com>
2024-08-22 10:30:51 +0800

    Adding markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 interface Props {
     params: { id: string }
 }
@@ -18,7 +19,9 @@ const IssueDetailPage = async ({ params }: Props) => {
                 <IssueStatusBadge status={issue.status} />
                 <Text>{issue.createAt.toDateString()}</Text>
             </Flex>
-            <Card>{issue.description}</Card>
+            <Card className='prose' mt="4">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
         </div>
     )
 }
